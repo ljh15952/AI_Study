2669aba78e86996a83a9cf9438e164b58532325f
중화이 <ljh15952@gmail.com>
2022-06-21 10:57:40 +0000

    6/21 19:57

--- src/study_1.py
+++ src/study_1.py
@@ -1,1 +1,11 @@
-print("hihihi?")
+#파이썬 활용
+#파이썬의 장점
+#간단함, 라이브러리 활용, 설치 간단
+#개발은 거인위의 올라탄 것 처럼
+#쥬피터 노트북
+#tensorflow 설치
+#pip install tensorflow==2.5.0
+#tensorflow 설치 확인
+#phython
+#import tensorflow as tf
+#tf.__version__
